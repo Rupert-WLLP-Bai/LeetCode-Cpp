a6824708bc8bb58a3fbcd19c19b13a1b7681c893
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2024-01-22 19:43:28 +0800

    p17 电话号码的字母组合

--- src/carl-200/mid/17.cpp
+++ src/carl-200/mid/17.cpp
@@ -14,24 +14,38 @@ class Solution {
      * @return vector<string> 所有可能的字母组合
      */
     vector<string> letterCombinations(string digits) {
-        // 字符和字符串相对应
-        const unordered_multimap<char, string> m({{'2', "abc"},
-                                                  {'3', "def"},
-                                                  {'4', "ghi"},
-                                                  {'5', "jkl"},
-                                                  {'6', "mno"},
-                                                  {'7', "pqrs"},
-                                                  {'8', "tuv"},
-                                                  {'9', "wxyz"}});
-
-        
+        if (digits.empty()) {
+            return {};
+        }
+        backtrack(0, digits);
+        return result;
     }
 
   private:
-    vector<vector<int>> result; // 最终结果
-    vector<int> path;           // 符合条件的结果
+    vector<string> result; // 最终结果
+    string path;           // 中间结果
+    const unordered_multimap<char, string> m = {
+        {'2', "abc"}, {'3', "def"},  {'4', "ghi"}, {'5', "jkl"},
+        {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
+    void backtrack(int index, const string &digits) {
+        // index 表示遍历到第几个数字
+        // 结束条件
+        if (index == digits.size()) {
+            result.push_back(path);
+            return;
+        }
 
-    void backtrack() {
+        // 用index取对应的字符集
+        char digit = digits[index];
+        auto iter = m.find(digit); // 找到对应的字符集
+        string str = iter->second; // 取出字符集
+        // 遍历字符集
+        for (int i = 0; i < str.size(); i++) {
+            path.push_back(str[i]);
+            backtrack(index + 1, digits);
+            path.pop_back();
+        }
+
     }
 };
 
